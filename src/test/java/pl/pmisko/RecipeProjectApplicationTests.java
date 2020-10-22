ab8ef3d1ae53337d2a5541038318a4870558367c
pmisko <miskopiotr@gmail.com>
2020-10-22 11:37:30 +0200

    initial commit

---
+++ src/test/java/pl/pmisko/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pmisko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
